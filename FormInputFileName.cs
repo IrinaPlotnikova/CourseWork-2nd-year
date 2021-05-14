471a2e5186a5244f5d47e51c56e4596168d41ba2
IrinaPlotnikova <plotnikova1029@gmail.com>
2021-05-14 13:20:28 +0300

    Adds comments in FormInputFileName.cs

--- FormInputFileName.cs
+++ FormInputFileName.cs
@@ -19,16 +19,19 @@ namespace CourseWorkSort
             Input = "";
         }
 
+        // введенное пользователем название
         public string Input { get; private set; }
 
+        // обработка нажатия на клавишу при вводе название
         private void tbxFilename_KeyPress(object sender, KeyPressEventArgs e)
         {
-            string forbiddenSymbols = "\\/:*?\"<>|";
+            string forbiddenSymbols = "\\/:*?\"<>|"; // запрещенные символы
             e.Handled = forbiddenSymbols.Contains(e.KeyChar) ||
-                        tbxFilename.Text == "" && e.KeyChar == ' ' ||
-                        tbxFilename.Text.Length == 31 && e.KeyChar != (char)Keys.Back;
+                        e.KeyChar == ' ' && tbxFilename.Text == "" ||
+                        e.KeyChar != (char)Keys.Back && tbxFilename.Text.Length == 31;
         }
 
+        // обработка кнопки подтверждения ввода
         private void btnOk_Click(object sender, EventArgs e)
         {
             string filename = tbxFilename.Text.TrimEnd();
@@ -37,7 +40,7 @@ namespace CourseWorkSort
                 MessageBox.Show("Название файла не должно быть пустой строкой.",
                                 "Некорректный ввод", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else
+            else // если имя файла не пусто
             {
                 Input = filename;
                 DialogResult = DialogResult.Yes;
